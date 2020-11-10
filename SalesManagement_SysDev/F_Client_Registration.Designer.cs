a18ebd562a746dc2bd38bb508f131d6e3eb6df23
KMasasi96 <masasi090625252@gmail.com>
2020-11-10 16:17:39 +0900

    顧客情報登録のプッシュテスト（ブランチ）

--- SalesManagement_SysDev/F_Client_Registration.Designer.cs
+++ SalesManagement_SysDev/F_Client_Registration.Designer.cs
@@ -555,7 +555,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(352, 344);
+            this.button1.Location = new System.Drawing.Point(346, 342);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(178, 60);
             this.button1.TabIndex = 11;
