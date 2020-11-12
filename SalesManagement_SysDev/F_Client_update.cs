0129e0aaba129d86ae1ff27dc3feb74b4d133e20
KMasasi96 <masasi090625252@gmail.com>
2020-11-12 14:50:06 +0900

    顧客情報更新

---
+++ SalesManagement_SysDev/F_Client_update.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesManagement_SysDev
+{
+    public partial class F_Client_update : Form
+    {
+        public F_Client_update()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
