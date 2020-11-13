a64b0a77c6a7a08071702443c3bdee70e9d43452
b9019 <b9019@oic.jp>
2020-11-13 16:46:20 +0900

    妥当な商品データ取得（商品データの形式チェック）適否判断まで

--- SalesManagement_SysDev/F_Product_regist.cs
+++ SalesManagement_SysDev/F_Product_regist.cs
@@ -70,17 +70,132 @@ namespace SalesManagement_SysDev
             }
         }
 
+        // 
+        //
+        //4.1.1　妥当な商品データ取得（新規登録）
+        //
+        //
+        private bool Get_Product_Data_AtRegistration()
+        {
+            // 商品データの形式チェック
+            string errorMessage = string.Empty;
+
+            // メーカIDの適否
+            if (!String.IsNullOrEmpty(txt_MaID.Text))
+            {
+                MessageBox.Show("メーカIDは入力できません");
+                MaID.Focus();
+                return false;
+            }
+
+            // 商品IDの適否
+            if (String.IsNullOrEmpty(txt_PrID.Text))
+            {
+                MessageBox.Show("商品IDは必須項目です");
+                txt_PrID.Focus();
+                return false;
+            }
+
+            // 商品名の適否
+            if (String.IsNullOrEmpty(txt_PrID.Text))
+            {
+                MessageBox.Show("商品IDは必須項目です");
+                txt_PrID.Focus();
+                return false;
+            }
+            //　JANコードの適否
+            if (String.IsNullOrEmpty(txt_PrJCode.Text))
+            {
+                MessageBox.Show("JANコードは必須項目です");
+                txt_PrJCode.Focus();
+                return false;
+            }
+            // 小分類IDの適否
+            if (String.IsNullOrEmpty(txt_ScID.Text))
+            {
+                MessageBox.Show("小分類IDは必須項目です");
+                txt_ScID.Focus();
+                return false;
+            }
+            // 型番の適否
+            if (String.IsNullOrEmpty(txt_PrModelNumber.Text))
+            {
+                MessageBox.Show("型番は必須項目です");
+                txt_PrModelNumber.Focus();
+                return false;
+            }
+            //　色の適否
+            if (String.IsNullOrEmpty(txt_PrColor.Text))
+            {
+                MessageBox.Show("色は必須項目です");
+                txt_PrColor.Focus();
+                return false;
+            }
+            // 発売日の適否
+            if (String.IsNullOrEmpty(txt_PrReleaseDate.Text))
+            {
+                MessageBox.Show("発売日は必須項目です");
+                txt_PrReleaseDate.Focus();
+                return false;
+            }
+            //　価格の適否
+            if (String.IsNullOrEmpty(txt_Price.Text))
+            {
+                MessageBox.Show("価格は必須項目です");
+                txt_Price.Focus();
+                return false;
+            }
+            //　安全在庫数の適否
+            if (String.IsNullOrEmpty(txt_PrSafetyStock.Text))
+            {
+                MessageBox.Show("安全在庫数は必須項目です");
+                txt_PrSafetyStock.Focus();
+                return false;
+            }
+
+            if (!_ic.FullWidthCharCheck(textBoxDivisionName.Text, out errorMessage))
+            {
+                MessageBox.Show(errorMessage);
+                textBoxDivisionName.Focus();
+                return false;
+            }
+
+            if (textBoxDivisionName.TextLength > 50)
+            {
+                MessageBox.Show("部署名は50文字以下です");
+                textBoxDivisionName.Focus();
+                return false;
+            }
+
+            // 削除フラグの適否
+            if (checkBoxDelFLG.CheckState == CheckState.Indeterminate)
+            {
+                MessageBox.Show("不確定になっています");
+                checkBoxDelFLG.Focus();
+                return false;
+            }
+
+            // 備考の適否
+            if (textBoxComments.TextLength > 80)
+            {
+                MessageBox.Show("備考は80文字以下です");
+                textBoxComments.Focus();
+                return false;
+            }
+
+            return true;
+
+        }
+
+
+
         private void btn_regist_Click(object sender, EventArgs e)
         {
-            // 
-            //
-            //
-            //
-            //
             if (!Get_Product_Data_AtRegistration())
             {
                 return;
             }
+
         }
     }
 }
