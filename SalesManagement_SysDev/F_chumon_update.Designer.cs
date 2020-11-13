98c75c3db595767a3d2ea11e2920b8b00e103b4c
b9131@oic.jp <b9131@oic.jp>
2020-11-13 15:09:50 +0900

    注文テスト

---
+++ SalesManagement_SysDev/F_chumon_update.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SalesManagement_SysDev
+{
+    partial class F_chumon_update
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // F_chumon_update
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 614);
+            this.Name = "F_chumon_update";
+            this.Text = "F_chumon_update";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
