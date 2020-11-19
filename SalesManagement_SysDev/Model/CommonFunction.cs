31bee4df5dd4cb9c6a4ed4741d4e689787e2979d
b9019 <b9019@oic.jp>
2020-11-19 14:38:47 +0900

    アクセス権のコーデイングをした。

--- SalesManagement_SysDev/Model/CommonFunction.cs
+++ SalesManagement_SysDev/Model/CommonFunction.cs
@@ -24,74 +24,74 @@ namespace SalesManagement_SysDev.Model
             // 色取得
             // in       sw : 色名
             // out      Colorオブジェクト
-            public Color GetColor(string sw)
-            {
-                switch (sw)
-                {
-                    case Constants.clrWhite:
-                        return Color.White;
-                    case Constants.clrIvory:
-                        return Color.Ivory;
-                    case Constants.clrLightYellow:
-                        return Color.LightYellow;
-                    case Constants.clrLightGray:
-                        return Color.LightGray;
-                    case Constants.clrLightPink:
-                        return Color.LightPink;
-                    case Constants.clrLightBlue:
-                        return Color.LightBlue;
-                    case Constants.clrAquamarine:
-                        return Color.Aquamarine;
-                    case Constants.clrLightSkyBlue:
-                        return Color.LightSkyBlue;
-                    case Constants.clrSkyBlue:
-                        return Color.SkyBlue;
-                    case Constants.clrPaleGreen:
-                        return Color.PaleGreen;
-                    case Constants.clrLightSalmon:
-                        return Color.LightSalmon;
-                    case Constants.clrLightGreen:
-                        return Color.LightGreen;
-                    case Constants.clrBlack:
-                        return Color.Black;
-                    case Constants.clrPink:
-                        return Color.Pink;
-                    case Constants.clrViolet:
-                        return Color.Violet;
-                    case Constants.clrSilver:
-                        return Color.Silver;
-                    case Constants.clrYellow:
-                        return Color.Yellow;
-                    case Constants.clrGold:
-                        return Color.Gold;
-                    case Constants.clrOrange:
-                        return Color.Orange;
-                    case Constants.clrGray:
-                        return Color.Gray;
-                    case Constants.clrOlive:
-                        return Color.Olive;
-                    case Constants.clrPurple:
-                        return Color.Purple;
-                    case Constants.clrBlue:
-                        return Color.Blue;
-                    case Constants.clrLime:
-                        return Color.Lime;
-                    case Constants.clrRed:
-                        return Color.Red;
-                    case Constants.clrBrown:
-                        return Color.Brown;
-                    case Constants.clrMaroon:
-                        return Color.Maroon;
-                    case Constants.clrNavy:
-                        return Color.Navy;
-                    case Constants.clrGreen:
-                        return Color.Green;
-                    case Constants.clrDarkGreen:
-                        return Color.DarkGreen;
-                    default:
-                        return Color.Black;
-                }
-            }
+            //public Color GetColor(string sw)
+            //{
+            //    switch (sw)
+            //    {
+            //        case Constants.clrWhite:
+            //            return Color.White;
+            //        case Constants.clrIvory:
+            //            return Color.Ivory;
+            //        case Constants.clrLightYellow:
+            //            return Color.LightYellow;
+            //        case Constants.clrLightGray:
+            //            return Color.LightGray;
+            //        case Constants.clrLightPink:
+            //            return Color.LightPink;
+            //        case Constants.clrLightBlue:
+            //            return Color.LightBlue;
+            //        case Constants.clrAquamarine:
+            //            return Color.Aquamarine;
+            //        case Constants.clrLightSkyBlue:
+            //            return Color.LightSkyBlue;
+            //        case Constants.clrSkyBlue:
+            //            return Color.SkyBlue;
+            //        case Constants.clrPaleGreen:
+            //            return Color.PaleGreen;
+            //        case Constants.clrLightSalmon:
+            //            return Color.LightSalmon;
+            //        case Constants.clrLightGreen:
+            //            return Color.LightGreen;
+            //        case Constants.clrBlack:
+            //            return Color.Black;
+            //        case Constants.clrPink:
+            //            return Color.Pink;
+            //        case Constants.clrViolet:
+            //            return Color.Violet;
+            //        case Constants.clrSilver:
+            //            return Color.Silver;
+            //        case Constants.clrYellow:
+            //            return Color.Yellow;
+            //        case Constants.clrGold:
+            //            return Color.Gold;
+            //        case Constants.clrOrange:
+            //            return Color.Orange;
+            //        case Constants.clrGray:
+            //            return Color.Gray;
+            //        case Constants.clrOlive:
+            //            return Color.Olive;
+            //        case Constants.clrPurple:
+            //            return Color.Purple;
+            //        case Constants.clrBlue:
+            //            return Color.Blue;
+            //        case Constants.clrLime:
+            //            return Color.Lime;
+            //        case Constants.clrRed:
+            //            return Color.Red;
+            //        case Constants.clrBrown:
+            //            return Color.Brown;
+            //        case Constants.clrMaroon:
+            //            return Color.Maroon;
+            //        case Constants.clrNavy:
+            //            return Color.Navy;
+            //        case Constants.clrGreen:
+            //            return Color.Green;
+            //        case Constants.clrDarkGreen:
+            //            return Color.DarkGreen;
+            //        default:
+            //            return Color.Black;
+            //    }
+            //}
 
             // 文字数で文字列を分割
             // in       strsrc : 入力文字列
@@ -122,15 +122,21 @@ namespace SalesManagement_SysDev.Model
             // アクセス権ラベル取得
             // in       accessAuth : アクセス権レベル
             // out      string
-            // 0 = 一般、1 = 管理者、2 = 特権管理者
+            // 0 = 営業　1 = 物流　２＝　本社　３＝　顧客、４＝　管理者５ = 特権管理者　
             public string GetAccessAuth(int accessAuth)
             {
                 switch (accessAuth)
                 {
-                    case Constants.numGeneral:
-                        return Constants.strGeneral;
+                    case Constants.numSales:
+                        return Constants.strSales:
+                    case Constants.numlogiManager:
+                        return Constants.strlogiManager;
+                    case Constants.numOffice:
+                        return Constants.strOffice;
+                    case Constants.numClient:
+                        return Constants.strClient;
                     case Constants.numManager:
-                        return Constants.strManager;
+                        return Constants.strManager:
                     case Constants.numMaster:
                         return Constants.strMaster;
                     default:
