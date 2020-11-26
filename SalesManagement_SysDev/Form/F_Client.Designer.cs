2c5e715e65ad54b9046450d29af007cb795baeb6
B9186 <B9186@oic.co.jp>
2020-11-26 19:25:41 +0900

    クライアントエラーメッセージ修正

--- SalesManagement_SysDev/Form/F_Client.Designer.cs
+++ SalesManagement_SysDev/Form/F_Client.Designer.cs
@@ -238,6 +238,7 @@
             this.button6.TabIndex = 162;
             this.button6.Text = "F2 登録";
             this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // menuStrip2
             // 
